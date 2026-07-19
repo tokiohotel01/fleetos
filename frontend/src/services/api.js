45c6d9e74fda89310acb22ab34d39890fc68b27c
Bianca Manuel <manuelbianca0808@gmail.com>
2026-07-18 21:57:21 -0300

    setup endpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({ baseURL: (process.env.REACT_APP_API_URL || '') + '/api' });
 api.interceptors.request.use(cfg => {
   const t = localStorage.getItem('token');
   if (t) cfg.headers.Authorization = 'Bearer ' + t;
